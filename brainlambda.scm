95f4748c76f666a4a938e115da2079802272237e
Tiago Campos <itotimcampos@windowslive.com>
2022-07-22 21:31:59 -0300

    Update brainlambda.scm

--- brainlambda.scm
+++ brainlambda.scm
@@ -1,8 +1,7 @@
-;Instructions of brainfuck code is defined in instructions_lis closure
+;Instructions of brainfuck code are defined in instructions_lis closure.
 ;instructions_lis is a list that recevied the functions of instructions
 ;the function of interpreter catch dynamic all values of Instructions and run
-
-;only exist closures in variables(anonymous function) for Instructions and to descrive the intruction, however, all functions is represented in fully pure of lambda calculus, of course case you want you can discard theses variables
+;Only exist closures in variables(anonymous function) for Instructions and to describe the instruction, however, all functions is represented in fully pure of lambda calculus, of course case you want you can discard theses variables
 
 ;EFFECTS I/O
 ;(display str) 2x
